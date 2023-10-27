2e3c1f8f53a948416bda6db5a29feb2b4b861993
sooolyv <146958137+sooolyv@users.noreply.github.com>
2023-10-27 06:33:59 -1100

    Add files via upload

---
+++ youngecnomy.html/script.js
@@ -0,0 +1,19 @@
+//tomamos los elementos
+
+var inputTarea = document.getElementById("tarea");
+var btn = document.getElementById("agregar");
+var listado = document.getElementById("listado");
+var cantidad = document.getElementById("cantidad");
+
+//variable que lleva la cantidad d tareas
+
+var total= 0;
+
+//funcion que agrega la tarea al listado
+
+btn.onclick = function() {
+//tomamos el valor del campo
+var elemento = inputarea.value;
+//creo un elemento li
+var li
+}
