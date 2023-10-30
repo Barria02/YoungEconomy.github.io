6bde8e4bb116b940e3eeb7bd8faff6b6c55cb4ac
sooolyv <146958137+sooolyv@users.noreply.github.com>
2023-10-29 14:14:26 -1100

    Add files via upload

--- youngecnomy.html/script.js
+++ youngecnomy.html/script.js
@@ -1,19 +1,44 @@
 //tomamos los elementos
-
 var inputTarea = document.getElementById("tarea");
 var btn = document.getElementById("agregar");
 var listado = document.getElementById("listado");
 var cantidad = document.getElementById("cantidad");
 
 //variable que lleva la cantidad d tareas
-
-var total= 0;
+var total = 0;
 
 //funcion que agrega la tarea al listado
-
 btn.onclick = function() {
+    //controlamos si el campo esta vacio
+    if (inputTarea.value =="") {
+        return;
+    }
 //tomamos el valor del campo
-var elemento = inputarea.value;
+var elemento = inputTarea.value;
 //creo un elemento li
-var li
+var li = document.createElement("li");
+li.textContent = elemento;
+//agrego el li al listado
+listado.appendChild(li);
+
+//incrmento la cantidad de tareas
+
+total++;
+cantidad.innerHTML = total;
+
+//agregamos el boton eliminar a cada elemento li
+
+var btnEliminar = document.createElement("span");
+btnEliminar.textContent = "\u00d7";
+li.appendChild(btnEliminar);
+
+//agregamos la uncionalidad que elimina la tarea del listado
+btnEliminar.onclick = function()  {
+    li.remove();
+    total--;
+    cantidad.innerHTML = total;
+}
+
+//limpiamos el campo input
+inputTarea.value = "";
 }
